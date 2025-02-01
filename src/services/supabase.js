81688e0f6ba2b404f5b6391eaff818bcee01e89e
ChanvirappaPrachande <chanvirappap@gmail.com>
2025-02-01 08:10:36 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://xbhxdqwcajlmybkazgku.supabase.co";
 
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhiaHhkcXdjYWpsbXlia2F6Z2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NDE0NzMsImV4cCI6MjA1MjQxNzQ3M30.MrnqmI2vo1PlOjAfgeN7RapWGe3mZbg9HjSta4kbnko";
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";//or use env 
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
